25f4bbbdebcf4f4760ecf660732074f7fe89b55a
TatianaNikitina <nikitina.tania.msu@gmail.com>
2023-08-31 23:26:46 +0300

    fix ex3

--- Sem2/HW2EX3.py
+++ Sem2/HW2EX3.py
@@ -1,8 +1,7 @@
 # Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
 import math
 
-N = 30
-print(math.log2(N))
+N = 50
 pow2List = [2**n for n in range(int(math.log2(N))+1)]
 print(pow2List)
 
